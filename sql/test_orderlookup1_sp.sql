462858947fd0192ce8c76e10ce23615f10b51b2a
Venu <dvenu@carolina.rr.com>
2015-10-30 15:02:34 -0400

    packaged all the procedures

--- sql/test_orderlookup1_sp.sql
+++ sql/test_orderlookup1_sp.sql
@@ -31,9 +31,9 @@ to_zip := '';
     --DBMS_OUTPUT.PUT_LINE(l_col_data.count);
     --  insert_mydata(l_col_data);
 
-      ORDER_PROCESS_LOOKUP (from_city, from_state, from_zip, to_city, to_state, to_zip,  instructions, l_col_data, order_id, message);
+   --   ORDER_PROCESS_LOOKUP (from_city, from_state, from_zip, to_city, to_state, to_zip,  instructions, l_col_data, order_id, message);
   --  ORDER_PROCESS_PROC1    ( 'COLORADAO SPRINGS', '80817',   'CO', 'Honolulu', '96621', 'HI', l_col_data, 'instructions', :order_id, :message);
-   -- VENU.ORDER_PROCESS_PROC('COLORADAO SPRINGS', '80817',   'CO', 'Honolulu', '96621', 'HI', sql_str, 'instructions', :order_id, :message);
+    VENU.ORDER_PROCESS_PACKAGE.SAVE_ORDER_VER_1_SP('COLORADAO SPRINGS',    'CO','80817', 'Honolulu', 'HI', '96621', sql_str, 'instructions', order_id, message);
    -- VENU.ORDER_PROCESS_PROC1('COLORADAO SPRINGS', '80817',   'CO', 'Honolulu', '96621', 'HI', sql_str, 'instructions', :order_id, :message);
  
 END;
